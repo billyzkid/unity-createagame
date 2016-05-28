92b85418df9699997fca0ce86279fdb156ba2e0b
Will Allan <billyzkid@yahoo.com>
2016-05-27 17:09:24 -0700

    Player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody myRigidbody;
+    private Vector3 velocity;
+
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+
+    public void Move(Vector3 velocity)
+    {
+        this.velocity = velocity;
+    }
+
+    public void LookAt(Vector3 point)
+    {
+        var heightCorrectedPoint = new Vector3(point.x, transform.position.y, point.z);
+        transform.LookAt(heightCorrectedPoint);
+    }
+}
