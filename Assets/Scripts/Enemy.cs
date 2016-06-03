12887c3f8ee2d626169e7d65a05a1703e4942636
Will Allan <billyzkid@yahoo.com>
2016-06-02 17:29:24 -0700

    Enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent pathfinder;
+    private Transform target;
+
+    private void Start()
+    {
+        pathfinder = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        StartCoroutine(UpdatePath());
+    }
+
+    private void Update()
+    {
+    }
+
+    private IEnumerator UpdatePath()
+    {
+        const float refreshRate = 0.25f;
+
+        while (target != null)
+        {
+            var targetPosition = new Vector3(target.position.x, 0, target.position.z);
+            pathfinder.SetDestination(targetPosition);
+
+            yield return new WaitForSeconds(refreshRate);
+        }
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,16 +1,39 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    private float speed = 10;
+    public LayerMask collisionMask;
 
-    private void Update()
-    {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-    }
+    private float speed = 10;
 
     public void SetSpeed(float speed)
     {
         this.speed = speed;
+    }
+
+    private void Update()
+    {
+        var moveDistance = speed * Time.deltaTime;
+
+        CheckCollisions(moveDistance);
+        transform.Translate(Vector3.forward * moveDistance);
+    }
+
+    private void CheckCollisions(float moveDistance)
+    {
+        var ray = new Ray(transform.position, transform.forward);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide))
+        {
+            OnHitObject(hit);
+        }
+    }
+
+    private void OnHitObject(RaycastHit hit)
+    {
+        print(hit.collider.gameObject.name);
+        GameObject.Destroy(gameObject);
     }
 }
